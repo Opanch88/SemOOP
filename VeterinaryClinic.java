970472478bb43276954262bb579e5fc618453ca7
«Oleg» <Pan159dom159@yandex.ru>
2023-12-20 01:16:46 +0300

    I created a class called "Veterinary Clinic". Added a method to get all the animals. Added a method to get all animals that run. Added a method to add animals (one at a time or a list).

--- VeterinaryClinic.java
+++ VeterinaryClinic.java
@@ -1,3 +1,40 @@
-public class VeterinaryClinic {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class VeterinaryClinic {
+    private List<Animal> patients = new ArrayList<>();
+
+    /**
+     * Добавляет по одному пациенту
+     */
+    public void addPatient(Animal animal) {
+        patients.add(animal);
+    }
+
+    /**
+     * Добавляет пациентов списком
+     */
+    public void addPatients(Animal... animals) {
+        Collections.addAll(patients, animals);
+    }
+
+    /**
+     * Возвращает бегающих животных
+     */
+    public List<Goable> getGoables() {
+        List<Goable> result = new ArrayList<>(patients.size());
+        for (Animal animal : patients) {
+            if (animal instanceof Goable)
+                result.add((Goable) animal);
+        }
+        return result;
+    }
+
+    /**
+     * Возвращает список пациентов
+     */
+    public List<Animal> getPatients() {
+        return patients;
+    }
 }
