1ca5a716677e4a66fb857b5584f9a625f5447586
«Oleg» <Pan159dom159@yandex.ru>
2023-12-21 11:53:51 +0300

    Added methods of working with clinic staff

--- VeterinaryClinic.java
+++ VeterinaryClinic.java
@@ -4,7 +4,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class VeterinaryClinic {
-    private List<Animal> patients = new ArrayList<>();
+    /**
+     * Методы для пациентов
+     */
+    public List<Animal> patients = new ArrayList<>();
 
     /**
      * Добавляет по одному пациенту
@@ -40,7 +43,7 @@ public class VeterinaryClinic {
     }
 
     /**
-     * Возвращает летающих животных
+     * Возвращает летающих животных (через stream api)
      */
     public List<Flyable> getFlyable() {
         return patients.stream()
@@ -60,4 +63,27 @@ public class VeterinaryClinic {
         }
         return result;
     }
+
+    /**
+     * Методы для персонала
+     */
+    public List<Doctor> doctors;
+    public List<Nurse> nurses;
+
+    public VeterinaryClinic() {
+        this.doctors = new ArrayList<>();
+    }
+    public List<Doctor> getAllDoctors() {
+        return doctors;
+    }
+    public void addDoctor(Doctor doctor) {
+        doctors.add(doctor);
+        System.out.println("Доктор " + doctor + " успешно добавлен");
+    }
+
+        public void addNurse (Nurse nurse){
+            doctors.add(nurse);
+            System.out.println("Сестра " + nurse + " успешно добавлен");
+        }
 }
+
