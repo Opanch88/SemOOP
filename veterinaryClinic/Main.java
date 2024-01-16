70bd4d0e62604774cd5c7daaf36430b50831c125
«Oleg» <Pan159dom159@yandex.ru>
2024-01-16 16:08:05 +0300

    .

--- veterinaryClinic/Main.java
+++ veterinaryClinic/Main.java
@@ -6,20 +6,20 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Cat barsik = new Cat("Barsik", LocalDate.of(1996, 7, 8), new ArrayList<>(), "Chumka", "Owner Alexei", 4);
+        veterinaryClinic.Cat barsik = new veterinaryClinic.Cat("Barsik", LocalDate.of(1996, 7, 8), new ArrayList<>(), "Chumka", "Owner Alexei", 4);
 //        Animal eagle = new Eagle("Kesha", LocalDate.of(2000, 8, 9), new ArrayList<>(), "Chumka", "Owner Alexei");
 //        Animal dog = new Dog("Bul", LocalDate.of(1988, 7, 2), new ArrayList<>(), "Stomach upset ", "Owner Oleg");
 //        Animal duck = new Duck("Donald", LocalDate.of(2001, 5, 8), new ArrayList<>(), "Avian flu", "Owner Petr");
-        Animal snake = new Snake("Kaa", LocalDate.of(1998, 5, 8), new ArrayList<>(), "Царапина", "Owner Petr");
-        Animal raven = new Raven("Kar-Karych", LocalDate.of(1997, 5, 8), new ArrayList<>(), "Is flying too high ", "Owner Sema");
+        veterinaryClinic.Animal snake = new veterinaryClinic.Snake("Kaa", LocalDate.of(1998, 5, 8), new ArrayList<>(), "Царапина", "Owner Petr");
+        veterinaryClinic.Animal raven = new veterinaryClinic.Raven("Kar-Karych", LocalDate.of(1997, 5, 8), new ArrayList<>(), "Is flying too high ", "Owner Sema");
 
-        Doctor doctor = new Doctor("Petr", "Хирург", 5);
-        Nurse nurse = new Nurse("Mila", "Интерн", 5);
-        Doctor stat = new Doctor("Petr", "Хирург", 5);
-        Nurse nurse2 = new Nurse("Mila", "Интерн", 5);
+        veterinaryClinic.Doctor doctor = new veterinaryClinic.Doctor("Petr", "Хирург", 5);
+        veterinaryClinic.Nurse nurse = new veterinaryClinic.Nurse("Mila", "Интерн", 5);
+        veterinaryClinic.Doctor stat = new veterinaryClinic.Doctor("Petr", "Хирург", 5);
+        veterinaryClinic.Nurse nurse2 = new veterinaryClinic.Nurse("Mila", "Интерн", 5);
 
 
-        VeterinaryClinic clinic = new VeterinaryClinic();
+        veterinaryClinic.VeterinaryClinic clinic = new veterinaryClinic.VeterinaryClinic();
         clinic.addPatients(barsik, snake, raven);
 
 //        System.out.println(clinic.getPatients());
